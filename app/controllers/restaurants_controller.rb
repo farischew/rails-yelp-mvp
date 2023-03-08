b2950c27baaa0af81184ed1c9a7634d4faf46527
Faris Chew <farischew@gmail.com>
2023-03-08 13:43:35 +0800

    Adds create to restaurant model

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,9 +11,24 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def create
+    @restaurant = Restaurant.create!(restaurant_params)
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params['id'])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(
+      :name,
+      :address,
+      :phone_number,
+      :category
+    )
   end
 end
